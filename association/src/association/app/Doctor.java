04ae204cdd206dd145a58d4bc1d12ce2bf3a3fc7
Poojakm <poojakm.xworkz@gmail.com>
2023-07-30 22:41:29 +0530

    association

---
+++ association/src/association/app/Doctor.java
@@ -0,0 +1,13 @@
+package association.app;
+
+public class Doctor {
+	public void stethoscope()
+	{
+		System.out.println("Invoking fever in Patient");
+	}
+	public void whiteCoat()
+	{
+		System.out.println("Invoking cough in Patient");
+	}
+
+}
---
+++ association/src/association/app/Mechanic.java
@@ -0,0 +1,13 @@
+package association.app;
+
+public class Mechanic {
+	public void name()
+	{
+		System.out.println("Invoking name in Mechanic");
+	}
+	public void shop()
+	{
+		System.out.println("Invoking shop in Mechanic");
+	}
+
+}
